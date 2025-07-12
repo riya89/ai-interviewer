cc247c666d7c831b2b7d6e918dbaac175f4f13b4
Riya Banik <riyabanik@Riyas-MacBook-Air.local>
2025-07-13 02:16:44 +0530

    home page ui

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 import {ReactNode} from 'react'
+import Image from 'next/image'
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+        <Image src="/logo.svg" alt="logo" height={38} width={32} />
+        <h2 className='text-primary-100'>PrepWise</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
   )
 }
 export default RootLayout
--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Control, Controller, Field, FieldValue, FieldValues } from 'react-hook-form'
-import { FormControl, FormDescription, FormItem, FormLabel, FormMessage } from './ui/form';
+import { Control, Controller, FieldValues } from 'react-hook-form'
+import { FormControl, FormItem, FormLabel, FormMessage } from './ui/form';
 import { Input } from './ui/input';
 
 interface FormFieldProps<T extends FieldValues> {
