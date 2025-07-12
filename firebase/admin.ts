288b899a82b8d2e543b85be75076f04677c90784
Riya Banik <riyabanik@Riyas-MacBook-Air.local>
2025-07-13 02:58:06 +0530

    firebase setup

---
+++ firebase/admin.ts
@@ -0,0 +1,22 @@
+import { cert, getApps, initializeApp} from 'firebase-admin/app';
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+const initFirebaseAdmin = () => {
+    const apps = getApps();
+    if (apps.length === 0) {
+        // Import the functions you need from the SDKs you need
+       initializeApp({
+        credential: cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
+        })
+       })
+    }
+    return {
+    auth: getAuth(),
+    db: getFirestore(),
+  }
+}
+
+export const { auth, db } = initFirebaseAdmin();
