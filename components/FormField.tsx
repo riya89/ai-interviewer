42b16e3fc7bf55187af2c0ed02c23dad3eab219c
Riya Banik <riyabanik@Riyas-MacBook-Air.local>
2025-07-13 00:04:22 +0530

    ready signin and signup

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -20,7 +20,7 @@ const FormField = ({ control, name, label, placeholder, type="text" }: FormField
       <FormItem>
         <FormLabel className='label'>{label}</FormLabel>
         <FormControl>
-          <Input className='input' placeholder={placeholder} {...field} />
+          <Input className='input' placeholder={placeholder} {...field} type={type} />
         </FormControl>
         
         <FormMessage />
